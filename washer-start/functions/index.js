0a21e103128c4bdd12030d0ecd37dfb0f1cea20f
MariaExathought <mariazeba.kurpalis@exathought.com>
2023-02-21 18:30:33 +0530

    [maria] added onSync method

--- washer-start/functions/index.js
+++ washer-start/functions/index.js
@@ -14,30 +14,30 @@
  * limitations under the License.
  */
 
-'use strict';
+"use strict";
 
-const functions = require('firebase-functions');
-const {smarthome} = require('actions-on-google');
-const {google} = require('googleapis');
-const util = require('util');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const { smarthome } = require("actions-on-google");
+const { google } = require("googleapis");
+const util = require("util");
+const admin = require("firebase-admin");
 // Initialize Firebase
 admin.initializeApp();
-const firebaseRef = admin.database().ref('/');
+const firebaseRef = admin.database().ref("/");
 // Initialize Homegraph
 const auth = new google.auth.GoogleAuth({
-  scopes: ['https://www.googleapis.com/auth/homegraph'],
+  scopes: ["https://www.googleapis.com/auth/homegraph"],
 });
 const homegraph = google.homegraph({
-  version: 'v1',
+  version: "v1",
   auth: auth,
 });
 // Hardcoded user ID
-const USER_ID = '123';
+const USER_ID = "123";
 
 exports.login = functions.https.onRequest((request, response) => {
-  if (request.method === 'GET') {
-    functions.logger.log('Requesting login page');
+  if (request.method === "GET") {
+    functions.logger.log("Requesting login page");
     response.send(`
     <html>
       <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -52,7 +52,7 @@ exports.login = functions.https.onRequest((request, response) => {
       </body>
     </html>
   `);
-  } else if (request.method === 'POST') {
+  } else if (request.method === "POST") {
     // Here, you should validate the user account.
     // In this sample, we do not do that.
     const responseurl = decodeURIComponent(request.body.responseurl);
@@ -60,54 +60,88 @@ exports.login = functions.https.onRequest((request, response) => {
     return response.redirect(responseurl);
   } else {
     // Unsupported method
-    response.send(405, 'Method Not Allowed');
+    response.send(405, "Method Not Allowed");
   }
 });
 
 exports.fakeauth = functions.https.onRequest((request, response) => {
-  const responseurl = util.format('%s?code=%s&state=%s',
-      decodeURIComponent(request.query.redirect_uri), 'xxxxxx',
-      request.query.state);
+  const responseurl = util.format(
+    "%s?code=%s&state=%s",
+    decodeURIComponent(request.query.redirect_uri),
+    "xxxxxx",
+    request.query.state
+  );
   functions.logger.log(`Set redirect as ${responseurl}`);
   return response.redirect(
-      `/login?responseurl=${encodeURIComponent(responseurl)}`);
+    `/login?responseurl=${encodeURIComponent(responseurl)}`
+  );
 });
 
 exports.faketoken = functions.https.onRequest((request, response) => {
-  const grantType = request.query.grant_type ?
-    request.query.grant_type : request.body.grant_type;
+  const grantType = request.query.grant_type
+    ? request.query.grant_type
+    : request.body.grant_type;
   const secondsInDay = 86400; // 60 * 60 * 24
   const HTTP_STATUS_OK = 200;
   functions.logger.log(`Grant type ${grantType}`);
 
   let obj;
-  if (grantType === 'authorization_code') {
+  if (grantType === "authorization_code") {
     obj = {
-      token_type: 'bearer',
-      access_token: '123access',
-      refresh_token: '123refresh',
+      token_type: "bearer",
+      access_token: "123access",
+      refresh_token: "123refresh",
       expires_in: secondsInDay,
     };
-  } else if (grantType === 'refresh_token') {
+  } else if (grantType === "refresh_token") {
     obj = {
-      token_type: 'bearer',
-      access_token: '123access',
+      token_type: "bearer",
+      access_token: "123access",
       expires_in: secondsInDay,
     };
   }
-  response.status(HTTP_STATUS_OK)
-      .json(obj);
+  response.status(HTTP_STATUS_OK).json(obj);
 });
 
 const app = smarthome();
 
 app.onSync((body) => {
-  // TODO: Implement full SYNC response
-  return {};
+  return {
+    requestId: body.requestId,
+    payload: {
+      agentUserId: USER_ID,
+      devices: [
+        {
+          id: "washer",
+          type: "action.devices.types.WASHER",
+          traits: [
+            "action.devices.traits.OnOff",
+            "action.devices.traits.StartStop",
+            "action.devices.traits.RunCycle",
+          ],
+          name: {
+            defaultNames: ["My Washer"],
+            name: "Washer",
+            nicknames: ["Washer"],
+          },
+          deviceInfo: {
+            manufacturer: "Acme Co",
+            model: "acme-washer",
+            hwVersion: "1.0",
+            swVersion: "1.0.1",
+          },
+          willReportState: true,
+          attributes: {
+            pausable: true,
+          },
+        },
+      ],
+    },
+  };
 });
 
 const queryFirebase = async (deviceId) => {
-  const snapshot = await firebaseRef.child(deviceId).once('value');
+  const snapshot = await firebaseRef.child(deviceId).once("value");
   const snapshotVal = snapshot.val();
   // TODO: Define device states to return
   return {};
@@ -135,7 +169,7 @@ app.onExecute((body) => {
 });
 
 app.onDisconnect((body, headers) => {
-  functions.logger.log('User account unlinked from Google Assistant');
+  functions.logger.log("User account unlinked from Google Assistant");
   // Return empty response
   return {};
 });
@@ -143,7 +177,7 @@ app.onDisconnect((body, headers) => {
 exports.smarthome = functions.https.onRequest(app);
 
 exports.requestsync = functions.https.onRequest(async (request, response) => {
-  response.set('Access-Control-Allow-Origin', '*');
+  response.set("Access-Control-Allow-Origin", "*");
   functions.logger.info(`Request SYNC for user ${USER_ID}`);
 
   // TODO: Call HomeGraph API for user '123'
@@ -154,10 +188,10 @@ exports.requestsync = functions.https.onRequest(async (request, response) => {
  * Send a REPORT STATE call to the homegraph when data for any device id
  * has been changed.
  */
-exports.reportstate = functions.database.ref('{deviceId}').onWrite(
-    async (change, context) => {
-      functions.logger.info('Firebase write event triggered Report State');
+exports.reportstate = functions.database
+  .ref("{deviceId}")
+  .onWrite(async (change, context) => {
+    functions.logger.info("Firebase write event triggered Report State");
 
-      // TODO: Get latest state and call HomeGraph API
-    });
-
+    // TODO: Get latest state and call HomeGraph API
+  });
